7ef725fff9a5ab7804c9a9e9dc7675090438b268
Alexander <jayero@mail.ru>
2022-08-16 19:46:52 +0300

    решение

---
+++ Задача10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numb = num % 100 / 10;
+Console.Write ("вторая цифра числа -> ");
+Console.WriteLine (numb);
